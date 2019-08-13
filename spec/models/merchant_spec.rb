969166bd4efd08f54e1023e386359470361278ed
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-13 15:05:57 -0600

    Passes model specs

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,8 +6,4 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:customers).through(:invoices) }
     it { should have_many :items }
   end
-
-  describe "validations" do
-    it { should validate_presence_of :name }
-  end
 end
