ec93e73b767ef02d93a277dec7feafc0e7515404
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-13 19:03:29 -0600

    Adds index and show

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -2,19 +2,43 @@ require 'rails_helper'
 
 describe "Merchant API" do
   before :each do
-    create_list(:merchant, 10)
-    get "/api/v1/merchants"
+    @merch1 = create(:merchant)
+    @merch2 = create(:merchant)
+    @merch3 = create(:merchant)
+    @merch4 = create(:merchant)
+    @merch5 = create(:merchant)
+    @merch6 = create(:merchant)
+    @merch7 = create(:merchant)
+    @merch8 = create(:merchant)
+
+    @id = @merch1.id
   end
 
   it "returns index of merchants" do
 
+    get "/api/v1/merchants"
+
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body)
-    
-    expect(merchants.count).to eq(10)
+
+    expect(merchants.count).to eq(8)
     expect(merchants.first["name"]).to be_kind_of(String)
     expect(merchants.first["created_at"]).to be_kind_of(String)
     expect(merchants.first["id"]).to be_kind_of(Integer)
+  end
+
+  it "returns index of merchants" do
+
+    get "/api/v1/merchants/#{@id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["id"]).to eq(@id)
+    expect(merchant["name"]).to be_kind_of(String)
+    expect(merchant["created_at"]).to be_kind_of(String)
+    expect(merchant["id"]).to be_kind_of(Integer)
   end
 end
