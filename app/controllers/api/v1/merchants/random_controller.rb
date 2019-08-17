2885cbd767df1953aedbfa39c6dd701b0af2d286
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-17 14:33:54 -0600

    Got a depreciation warning about random method, changed to all.sample

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RandomController < ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.order('RANDOM()').first)
+    render json: MerchantSerializer.new(Merchant.all.sample)
   end
 end
