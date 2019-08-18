9a5581ba9bcb7b174e672a583772651bac311163
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-18 17:20:14 -0600

    Adds totla items sold

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,4 +10,13 @@ class Merchant < ApplicationRecord
       .order("revenue DESC")
       .limit(quantity)
   end
+
+  def self.total_items_sold(quantity)
+   joins(invoices: [:transactions, :invoice_items])
+     .where("transactions.result = ?", "success")
+     .select("merchants.*, SUM(invoice_items.quantity) AS items_sold")
+     .group("merchants.id")
+     .order("items_sold DESC")
+     .limit(quantity)
+  end
 end
